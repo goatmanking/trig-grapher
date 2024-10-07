1249dffccfbf559b829b837e0ebea8e110b71da2
mushtaq <afsarop9@outlook.com>
2024-10-07 16:45:37 +0500

    trigrapheralpha

--- main.py
+++ main.py
@@ -13,12 +13,23 @@ SCREEN_HEIGHT = 720
 pygame.display.set_caption("Trignometry Function Sketcher")
 clock = pygame.time.Clock()
 running = True
+
+
+
 font = pygame.font.Font('./trig-grapher/fonts/LycheeSoda.ttf', 24)
 font2 = pygame.font.Font('./trig-grapher/fonts/LycheeSoda.ttf', 12)
-angle_text_rect = pygame.Rect(200, 200, 142, 32)
-period_text_rect = pygame.Rect(500, 400, 142, 32)
-amplitude_text_rect = pygame.Rect(200, 600, 142, 32)
-b_text_rect = pygame.Rect(200, 400, 142, 32)
+font3 = pygame.font.Font('./trig-grapher/fonts/lemonmilk.otf', 18)
+titleFont  = pygame.font.Font('./trig-grapher/fonts/LycheeSoda.ttf', 72)
+font4 = pygame.font.Font('./trig-grapher/fonts/LycheeSoda.ttf', 44)
+
+
+background =pygame.image.load('./trig-grapher/assets/background.png') 
+background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+angle_text_rect = pygame.Rect(150, 200, 142, 32)
+period_text_rect = pygame.Rect(400, 200, 142, 32)
+amplitude_text_rect = pygame.Rect(690, 200, 142, 32)
+b_text_rect = pygame.Rect(1090, 200, 142, 32)
 
 def drawLine(w:int, h: int, angle:int, func:str, period:int):
     pygame.draw.line(screen, 'black', (0,h/2), (w,  h/2), 5)
@@ -28,7 +39,7 @@ def drawLine(w:int, h: int, angle:int, func:str, period:int):
     p = 90/period
 
     if p < 90:
-        angle *= period
+        angle = int (angle * period)
     elif p > 90:
         angle = int(angle * period)
 
@@ -48,16 +59,16 @@ def drawLine(w:int, h: int, angle:int, func:str, period:int):
                 prev_y = h/2 -(sine_value * h/2)
     elif func == 'c':
             prev_x  = 40
-            prev_y = h/2
+            prev_y = 0
             ratio_of_angle = (w-40)/angle
             for i in range(angle):
                 cos_value = math.cos(math.radians(i))
                 if cos_value > 0 or cos_value ==  math.cos(math.radians(0)):
-                    pygame.draw.line(screen, 'green',(prev_x, prev_y), ((i * ratio_of_angle) + 40, h/2 -(cos_value * h/2)))
+                    pygame.draw.line(screen, 'green',(prev_x, prev_y), ((i * ratio_of_angle) + 40, h/2 -(cos_value * h/2)), 3)
                     prev_x = (i * ratio_of_angle) + 40
                     prev_y = h/2 -(cos_value * h/2)
                 elif cos_value < 0:
-                    pygame.draw.line(screen, 'red',(prev_x, prev_y), ((i * ratio_of_angle) + 40,  h/2 -(cos_value * h/2)))
+                    pygame.draw.line(screen, 'red',(prev_x, prev_y), ((i * ratio_of_angle) + 40,  h/2 -(cos_value * h/2)), 3)
                     prev_x = (i * ratio_of_angle) + 40
                     prev_y = h/2 -(cos_value * h/2)
     elif func == 't':
@@ -127,6 +138,18 @@ def drawAmplitude(amplitude, b):
             screen.blit(max_font,(10, 10) )
             screen.blit(min_font,(10, 690))
 
+
+def isAnInteger(unicode):
+    number_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.']
+
+    for i in number_list:
+        if unicode == i:
+            return True
+        
+    return False
+
+
+
 func = 'c'
 ANGLE = 360
 ratio = SCREEN_WIDTH/360
@@ -146,15 +169,17 @@ selected_period_rect = False
 selected_amplitude_rect = False
 selected_b_rect = False
 
-angle_font =  font2.render(f'{angle_text}' ,False, (125, 0, 125))
-period_font =  font2.render(f'{period_text}' ,False,  (125, 0, 125))
-amplitude_font =  font2.render(f'{amplitude_text}' ,False,  (125, 0, 125))
-b_font =  font2.render(f'{b_text}' ,False,  (125, 0, 125))
+angle_font =  font2.render(f'{angle_text}' ,False, 'black')
+period_font =  font2.render(f'{period_text}' ,False,  'black')
+amplitude_font =  font2.render(f'{amplitude_text}' ,False,  'black')
+b_font =  font2.render(f'{b_text}' ,False,  'grey')
 
-a_font =  font2.render(f'Angle: ' ,False, 'black')
-p_font =  font2.render(f'Period: ' ,False,  'black')
-amp_font =  font2.render(f'Amplitude:' ,False,  'black')
-ba_font =  font2.render(f'B-value(use 0 if none): ' ,False, 'black')
+a_font =  font3.render(f'Angle: ' ,True, 'black')
+p_font =  font3.render(f'Period: ' ,True,  'black')
+amp_font =  font3.render(f'Amplitude:' ,True,  'black')
+ba_font =  font3.render(f'B-value(use 0 if none): ' ,True, 'black')
+
+delay_time = 0
 
 
 while running:
@@ -164,7 +189,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and not trig_graph_started:
             # print(f"Mouse coords: {pygame.mouse.get_pos()}\nAngle: {((pygame.mouse.get_pos()[0]) - 40) / ratio}")
             if angle_text_rect.collidepoint(pygame.mouse.get_pos()):
                 selected_angle_rect = True
@@ -179,51 +204,59 @@ while running:
                 selected_b_rect = False
                 selected_amplitude_rect = False
                 selected_period_rect = False
+            if generate_btn_rect.collidepoint(pygame.mouse.get_pos()):
+                print("generate")
+                ANGLE = int(angle_text)
+                PERIOD = float(period_text)
+                b = int(b_text)
+                AMPLITUDE = int(amplitude_text)
+                trig_graph_started = True
         if event.type == pygame.KEYDOWN and not trig_graph_started: # bro wtf is this code shouldve just made a function to make the code readibility better
-            if not (amplitude_font.get_width()) >= 100 and not (angle_font.get_width()) >= 100 and not (b_font.get_width()) >= 100 and not (period_font.get_width()) >= 100:
+          #  if not (amplitude_font.get_width()) >= 100 and not (angle_font.get_width()) >= 100 and not (b_font.get_width()) >= 100 and not (period_font.get_width()) >= 100:
+            if isAnInteger(event.unicode) or event.key == pygame.K_BACKSPACE:
+              
                 if selected_angle_rect:
                     if event.key == pygame.K_BACKSPACE:
                         angle_text = angle_text[:-1]
                     else:
                         angle_text += event.unicode
-                    angle_font =  font.render(f'{ angle_text}' ,False, (0, 255, 0))
+                    angle_font =  font.render(f'{ angle_text}' ,False,'black')
                     
                 elif selected_period_rect:
                     if event.key == pygame.K_BACKSPACE:
                         period_text = period_text[:-1]
                     else:
                         period_text += event.unicode
-                    period_font =  font.render(f'{period_text}' ,False, (0, 255, 0))
+                    period_font =  font.render(f'{period_text}' ,False, 'black')
                 elif selected_amplitude_rect :
                     if event.key == pygame.K_BACKSPACE:
                         amplitude_text = amplitude_text[:-1]
                     else:
                         amplitude_text += event.unicode
-                    amplitude_font =  font.render(f'{amplitude_text}' ,False, (0, 255, 0))
+                    amplitude_font =  font.render(f'{amplitude_text}' ,False, 'black')
                 elif selected_b_rect:
                     if event.key == pygame.K_BACKSPACE:
                         b_text = b_text[:-1]
                     else:
                         b_text += event.unicode
-                    b_font =  font.render(f'{b_text}' ,False, (0, 255, 0))
-            else:
-                if selected_angle_rect:
-                    if event.key == pygame.K_BACKSPACE:
-                        angle_text = angle_text[:-1]
-                    angle_font =  font.render(f'{ angle_text}' ,False, (0, 255, 0))
+                    b_font =  font.render(f'{b_text}' ,False, 'black')
+            # else:
+            #    if selected_angle_rect:
+            #        if event.key == pygame.K_BACKSPACE:
+            #            angle_text = angle_text[:-1]
+            #        angle_font =  font.render(f'{ angle_text}' ,False, (0, 255, 0))
                     
-                elif selected_period_rect:
-                    if event.key == pygame.K_BACKSPACE:
-                        period_text = period_text[:-1]
-                    period_font =  font.render(f'{period_text}' ,False, (0, 255, 0))
-                elif selected_amplitude_rect :
-                    if event.key == pygame.K_BACKSPACE:
-                        amplitude_text = amplitude_text[:-1]
-                    amplitude_font =  font.render(f'{amplitude_text}' ,False, (0, 255, 0))
-                elif selected_b_rect:
-                    if event.key == pygame.K_BACKSPACE:
-                        b_text = b_text[:-1]
-                    b_font =  font.render(f'{b_text}' ,False, (0, 255, 0))
+            #    elif selected_period_rect:
+            #        if event.key == pygame.K_BACKSPACE:
+            #            period_text = period_text[:-1]
+           #         period_font =  font.render(f'{period_text}' ,False, (0, 255, 0))
+            #    elif selected_amplitude_rect :
+           #        if event.key == pygame.K_BACKSPACE:
+            #        amplitude_font =  font.render(f'{amplitude_text}' ,False, (0, 255, 0))
+            #    elif selected_b_rect:
+            #        if event.key == pygame.K_BACKSPACE:
+            #            b_text = b_text[:-1]
+            #        b_font =  font.render(f'{b_text}' ,False, (0, 255, 0))
             
     if trig_graph_started:
 
@@ -239,28 +272,49 @@ while running:
             font_surf = font.render('Straight Red Lines indicate Asymptotes!', True, (0,0,0))
             screen.blit(font_surf, ((((SCREEN_WIDTH-40)/2) - 150), 600))
     else:
-        screen.fill("#989598")
-        if (angle_font.get_width() + 30) >= angle_text_rect.w:  angle_text_rect.w = angle_font.get_width() + 30
+        screen.blit(background, background.get_rect())
+        #if (angle_font.get_width() + 30) >= angle_text_rect.w:  angle_text_rect.w = angle_font.get_width() + 30
         pygame.draw.rect(screen, 'white', angle_text_rect, 0, 2)
-        screen.blit(a_font, (angle_text_rect.x - 20, angle_text_rect.y))
+        screen.blit(a_font, (angle_text_rect.x - 90, angle_text_rect.y))
 
-        if (period_font.get_width() + 30) >= period_text_rect.w:  period_text_rect.w = period_font.get_width() + 30
+       # if (period_font.get_width() + 30) >= period_text_rect.w:  period_text_rect.w = period_font.get_width() + 30
         pygame.draw.rect(screen, 'white', period_text_rect, 0, 2)
-        screen.blit(p_font, (period_text_rect.x - 20, period_text_rect.y))
+        screen.blit(p_font, (period_text_rect.x - 90, period_text_rect.y))
 
-        if (amplitude_font.get_width() + 30) >= amplitude_text_rect.w:  amplitude_text_rect.w = amplitude_font.get_width() + 30
+        #if (amplitude_font.get_width() + 30) >= amplitude_text_rect.w:  amplitude_text_rect.w = amplitude_font.get_width() + 30
         pygame.draw.rect(screen, 'white', amplitude_text_rect, 0, 2)
-        screen.blit(amp_font, (amplitude_text_rect.x - 20, amplitude_text_rect.y))
+        screen.blit(amp_font, (amplitude_text_rect.x - 130, amplitude_text_rect.y))
 
-        if (b_font.get_width() + 30) >= b_text_rect.w:  b_text_rect.w = b_font.get_width() + 30
+       # if (b_font.get_width() + 30) >= b_text_rect.w:  b_text_rect.w = b_font.get_width() + 30
         pygame.draw.rect(screen, 'white', b_text_rect, 0, 2)
-        screen.blit(ba_font, (b_text_rect.x - 20, b_text_rect.y))
+        screen.blit(ba_font, (b_text_rect.x - 240, b_text_rect.y))
 
+
+        generate_font = font4.render('Generate', True, 'white')
+
+        generate_btn_rect  = pygame.draw.rect(screen, 'lightgreen', (480, 640, 300, 42), 0, 8)
+        screen.blit(generate_font, ((generate_btn_rect.x) + 70, generate_btn_rect.y -3))
+
+        color = ['green', 'red', 'blue', 'orange', 'purple', 'magenta', 'yellow']
+        color_chosen_timestamp = time.time()
+
+        if delay_time == 0:
+            title_color = random.choice(color)
+            delay_time = time.time()
+        elif (color_chosen_timestamp -  delay_time) >= 0.75: 
+            title_color = random.choice(color)
+            delay_time = time.time()
+
+
+        title_surf = titleFont.render('Trignometric Graphs', True, title_color)
+
+        screen.blit(title_surf, (300, 0))
         screen.blit(angle_font, (angle_text_rect.x + 10, angle_text_rect.y + 5))
         screen.blit(period_font, (period_text_rect.x + 10, period_text_rect.y + 5))
         screen.blit(amplitude_font, (amplitude_text_rect.x + 10, amplitude_text_rect.y + 5))
         screen.blit(b_font, (b_text_rect.x + 10, b_text_rect.y + 5))
 
+    
 
     pygame.display.flip()
     clock.tick(60)  
